98e149819fd65805373d1ac4008af20d60799114
LSH <tmdghksgkqru@naver.com>
2024-04-15 11:27:27 +0900

    First Commit

---
+++ src/Ex01.java
@@ -0,0 +1,14 @@
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		System.out.println("팀장 첫번째 커밋");
+		System.out.println("팀장 두번째 커밋");
+		System.out.println("팀장 세번째 커밋");
+		System.out.println("팀원 네번째 커밋");
+		
+		
+
+	}
+
+}
